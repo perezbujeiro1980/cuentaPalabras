503117905f0472d24ca9c02654e7ce92551adae3
githubawaq <97857484+perezbujeiro1980@users.noreply.github.com>
2023-04-11 13:25:35 -0300

    ok

---
+++ ContadordePalabras/cuentaPalabras.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ContadordePalabras
+{
+    class cuentaPalabras
+    {
+        static void Main(string[] args)
+        {
+            string text = "Un texto que tiene palabras.";
+            int n=0;
+            text = Regex.Replace(text, @"\s+", " ").Trim();
+            var words = text.Split(' ');
+            n= words.Length;
+
+            Console.WriteLine("Nùmero de palabras: " + n);
+            Console.ReadKey();
+        }
+         
+    }
+}
